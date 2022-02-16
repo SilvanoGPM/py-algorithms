a6f2cab9ff2ff46158c4238ce8df6439a04791b5
SkyG0D <silvanosilvino@hotmail.com>
2022-02-15 21:50:32 -0300

    test: bubble sort test added

--- test/test_bubble_sort.py
+++ test/test_bubble_sort.py
@@ -1,21 +1,21 @@
 import unittest
+import sys
 
-from selection_sort import selection_sort
+from sorters.bubble_sort import bubble_sort
 
-
-class SelectionSortTest(unittest.TestCase):
-    def test_selection_sort_asc(self):
+class BubbleSortTest(unittest.TestCase):
+    def test_bubble_sort_asc(self):
         array = [4, 1, 6, 7, 3, 2]
         expected_array = [1, 2, 3, 4, 6, 7]
 
-        sorted_array = selection_sort(array)
+        sorted_array = bubble_sort(array)
 
         self.assertListEqual(sorted_array, expected_array)
 
-    def test_selection_sort_desc(self):
+    def test_bubble_sort_desc(self):
         array = [4, 1, 6, 7, 3, 2]
         expected_array = [7, 6, 4, 3, 2, 1]
 
-        sorted_array = selection_sort(array, asc=False)
+        sorted_array = bubble_sort(array, asc=False)
 
         self.assertListEqual(sorted_array, expected_array)
---
+++ test/test_selection_sort.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+
+from sorters.selection_sort import selection_sort
+
+class SelectionSortTest(unittest.TestCase):
+    def test_selection_sort_asc(self):
+        array = [4, 1, 6, 7, 3, 2]
+        expected_array = [1, 2, 3, 4, 6, 7]
+
+        sorted_array = selection_sort(array)
+
+        self.assertListEqual(sorted_array, expected_array)
+
+    def test_selection_sort_desc(self):
+        array = [4, 1, 6, 7, 3, 2]
+        expected_array = [7, 6, 4, 3, 2, 1]
+
+        sorted_array = selection_sort(array, asc=False)
+
+        self.assertListEqual(sorted_array, expected_array)
