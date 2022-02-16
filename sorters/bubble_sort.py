7320bb552431d53f1a9d1b51d2e01ec77b9b12bb
SkyG0D <silvanosilvino@hotmail.com>
2022-02-15 21:39:46 -0300

    feat: bubble sort added

---
+++ sorters/bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(array_to_sort: list, asc = True):
+    array = array_to_sort.copy()
+
+    n = len(array) - 1
+
+    for x in range(n):
+        for i in range(n):
+            comparation = array[i] > array[i + 1] if asc else array[i] < array[i + 1]
+            
+            if comparation:
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+
+    return array
